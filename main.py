7c0ab35be21f1252e095e2283696e7b0a6a192d4
riabov-vladimir <vriabovsvet@gmail.com>
2020-04-28 11:46:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# boys = ['Peter', 'Alex', 'John', 'Arthur', 'Richard']
+girls = ['Kate', 'Liza', 'Kira', 'Emma', 'Trisha']
+
+comparison = zip(sorted(boys), sorted(girls))
+
+if len(boys) == len(girls):
+  print(list(comparison))
+else:
+  print('Пара найдется или всем или никому!')
+
